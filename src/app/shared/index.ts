76092de7f5b7e5e66cbe6e286d2d76968124eb51
JulianaV <julianabalta16@gmail.com>
2022-02-24 22:31:54 -0300

    feat: shared part created and initiated some components

---
+++ src/app/shared/index.ts
@@ -0,0 +1,5 @@
+export * from '../shared/buttons/buttons.module';
+export * from '../shared/controls/controls.module';
+export * from '../shared/indicators/indicators.module';
+export * from '../shared/layouts/layouts.module';
+export * from '../shared/popups/popups.module';
